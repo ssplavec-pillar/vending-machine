eac3b684be17021a43c4095ab3d75227c8350273
Scott Splavec <scott.splavec@accenture.com>
2020-07-20 08:21:53 -0500

    Implement coin class

---
+++ src/models/coin.js
@@ -0,0 +1,11 @@
+export default class Coin {
+    name = '';
+    weight = 0;
+    size = 0;
+    value = 0;
+
+    constructor(weight, size) {
+        this.weight = weight;
+        this.size = size;
+    }
+}
